ee76ab2d1e908cd470010e07eff0496c3a9d23bd
Tristen <tristen.wentling@gmail.com>
2024-04-10 22:24:17 -0400

    updated to delta-spark 3.5.1, added tree

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ source "$HOME/.cargo/env"
 
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS='lab --ip=0.0.0.0'
-export DELTA_SPARK_VERSION='3.0.0'
+export DELTA_SPARK_VERSION='3.1.0'
 export DELTA_PACKAGE_VERSION=delta-spark_2.12:${DELTA_SPARK_VERSION}
 
 $SPARK_HOME/bin/pyspark --packages io.delta:${DELTA_PACKAGE_VERSION} \
